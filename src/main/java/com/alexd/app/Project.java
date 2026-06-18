f3b177e96c0e87f4ef45688a3c8dc1e9da13dabe
alexd-ev <alexdev719@gmail.com>
2026-06-18 16:49:46 +1000

    Add `Project.java` for extracting project information from database

---
+++ src/main/java/com/alexd/app/Project.java
@@ -0,0 +1,46 @@
+package com.alexd.app;
+
+public class Project {
+    private int id;
+    private String title;
+    private String description;
+    private String github_url;
+    private int experience_id;
+    private int education_id;
+
+    public Project() {
+    }
+
+    public Project(int id, String title, String description, String github_url, int experience_id, int education_id) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.github_url = github_url;
+        this.experience_id = experience_id;
+        this.education_id = education_id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getGithubUrl() {
+        return github_url;
+    }
+
+    public int getExperienceId() {
+        return experience_id;
+    }
+
+    public int getEducationId() {
+        return education_id;
+    }
+}
