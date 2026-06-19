8d9063b08aa57eefb97a9d73ae76fadbad916dda
alexd-ev <alexdev719@gmail.com>
2026-06-19 13:59:12 +1000

    Add `loadAllProjectSkills` method to map each project with their skills

--- src/main/java/com/alexd/app/Project.java
+++ src/main/java/com/alexd/app/Project.java
@@ -2,10 +2,6 @@ package com.alexd.app;
 
 import java.util.ArrayList;
 
-import java.sql.Statement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
 public class Project {
     private int id;
     private String title;
@@ -13,6 +9,7 @@ public class Project {
     private String github_url;
     private int experience_id;
     private int education_id;
+    private ArrayList<String> skills = new ArrayList<>();
 
     public Project() {
     }
@@ -50,17 +47,7 @@ public class Project {
         return education_id;
     }
 
-    public static ArrayList<Project> loadAllProjects(Statement statement) throws SQLException {
-        ArrayList<Project> projects = new ArrayList<>();
-        String projectsQuery = "SELECT * FROM projects";
-        try (ResultSet projectsResults = statement.executeQuery(projectsQuery)) {
-            while (projectsResults.next()) {
-                Project project = new Project(projectsResults.getInt("id"), projectsResults.getString("title"),
-                        projectsResults.getString("description"), projectsResults.getString("github_url"),
-                        projectsResults.getInt("experience_id"), projectsResults.getInt("education_id"));
-                projects.add(project);
-            }
-        }
-        return projects;
+    public ArrayList<String> getProjectSkills() {
+        return skills;
     }
 }
--- src/main/java/com/alexd/app/ProjectSkill.java
+++ src/main/java/com/alexd/app/ProjectSkill.java
@@ -1,5 +1,12 @@
 package com.alexd.app;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+
+import java.sql.Statement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class ProjectSkill {
     private int project_id;
     private int skill_id;
@@ -18,5 +25,38 @@ public class ProjectSkill {
 
     public int getSkillId() {
         return skill_id;
+    }
+
+    public static ArrayList<Project> loadAllProjectSkills(Statement statement) throws SQLException {
+        LinkedHashMap<Integer, Project> projectMap = new LinkedHashMap<>();
+        String projectSkillsQuery = """
+                SELECT p.id,
+                       p.title,
+                       p.description,
+                       p.github_url,
+                       p.experience_id,
+                       p.education_id,
+                       s.name
+                  FROM projects p
+                       LEFT JOIN
+                       project_skills ps ON p.id = ps.project_id
+                       LEFT JOIN
+                       skills s ON ps.skill_id = s.id
+                 ORDER BY p.id;
+                """;
+        try (ResultSet projectSkillsResults = statement.executeQuery(projectSkillsQuery)) {
+            while (projectSkillsResults.next()) {
+                int projectId = projectSkillsResults.getInt("id");
+                if (!projectMap.containsKey(projectId)) {
+                    Project project = new Project(projectId, projectSkillsResults.getString("title"),
+                            projectSkillsResults.getString("description"), projectSkillsResults.getString("github_url"),
+                            projectSkillsResults.getInt("experience_id"), projectSkillsResults.getInt("education_id"));
+                    projectMap.put(projectId, project);
+                }
+                String skillName = projectSkillsResults.getString("name");
+                projectMap.get(projectId).getProjectSkills().add(skillName);
+            }
+        }
+        return new ArrayList<>(projectMap.values());
     }
 }
