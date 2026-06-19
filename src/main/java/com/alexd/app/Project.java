13aae94babdd1a627e8734f64d661de1069d0fdc
alexd-ev <alexdev719@gmail.com>
2026-06-19 13:10:44 +1000

    Add `loadAllProjects` method to `Project.java`

--- src/main/java/com/alexd/app/Project.java
+++ src/main/java/com/alexd/app/Project.java
@@ -1,5 +1,11 @@
 package com.alexd.app;
 
+import java.util.ArrayList;
+
+import java.sql.Statement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class Project {
     private int id;
     private String title;
@@ -42,5 +48,19 @@ public class Project {
 
     public int getEducationId() {
         return education_id;
+    }
+
+    public static ArrayList<Project> loadAllProjects(Statement statement) throws SQLException {
+        ArrayList<Project> projects = new ArrayList<>();
+        String projectsQuery = "SELECT * FROM projects";
+        try (ResultSet projectsResults = statement.executeQuery(projectsQuery)) {
+            while (projectsResults.next()) {
+                Project project = new Project(projectsResults.getInt("id"), projectsResults.getString("title"),
+                        projectsResults.getString("description"), projectsResults.getString("github_url"),
+                        projectsResults.getInt("experience_id"), projectsResults.getInt("education_id"));
+                projects.add(project);
+            }
+        }
+        return projects;
     }
 }
