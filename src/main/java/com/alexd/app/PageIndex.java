863730920b7034581817952e856580e35b6dae40
alexd-ev <alexdev719@gmail.com>
2026-06-20 15:20:00 +1000

    Add `PageIndex.java`

---
+++ src/main/java/com/alexd/app/PageIndex.java
@@ -0,0 +1,17 @@
+package com.alexd.app;
+
+import java.util.HashMap;
+
+import io.javalin.http.Handler;
+import io.javalin.http.Context;
+
+public class PageIndex implements Handler {
+    public static final String INDEX_URL = "/";
+    private static final String INDEX_TEMPLATE = "templates/index.html";
+
+    @Override
+    public void handle(Context context) throws Exception {
+        HashMap<String, Object> model = new HashMap<String, Object>();
+        context.render(INDEX_TEMPLATE, model);
+    }
+}
