1a14d5ddd23c2d0e7f317f3d2135d0579cd638a2
alexd-ev <alexdev719@gmail.com>
2026-06-20 18:19:31 +1000

    Add custom `SQLException` error message

--- src/main/java/com/alexd/app/App.java
+++ src/main/java/com/alexd/app/App.java
@@ -15,6 +15,16 @@ public class App {
             config.staticFiles.add("public/");
             config.fileRenderer(new JavalinThymeleaf());
             config.routes.get(PageIndex.INDEX_URL, new PageIndex());
+            config.routes.exception(java.sql.SQLException.class, (e, context) -> {
+                System.err.println(e.getMessage());
+                context.status(500).html("""
+                        <div style='text-align:center; padding-top:50px; font-family:sans-serif;'>
+                            <h2>System Maintenance</h2>
+                            <p>My portfolio database is refreshing. Please wait a brief moment and
+                            reload.</p>
+                        </div>
+                        """);
+            });
         }).start(JAVALIN_PORT);
     }
 }
