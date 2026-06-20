f620316bed70f3e9e6105d2916792afe88782edf
alexd-ev <alexdev719@gmail.com>
2026-06-20 18:32:20 +1000

    Add generic `Exception` custom error message

--- src/main/java/com/alexd/app/App.java
+++ src/main/java/com/alexd/app/App.java
@@ -1,5 +1,7 @@
 package com.alexd.app;
 
+import java.sql.SQLException;
+
 import io.javalin.Javalin;
 import io.javalin.plugin.bundled.RouteOverviewPlugin;
 import io.javalin.rendering.template.JavalinThymeleaf;
@@ -15,15 +17,13 @@ public class App {
             config.staticFiles.add("public/");
             config.fileRenderer(new JavalinThymeleaf());
             config.routes.get(PageIndex.INDEX_URL, new PageIndex());
-            config.routes.exception(java.sql.SQLException.class, (e, context) -> {
+            config.routes.exception(SQLException.class, (e, context) -> {
                 System.err.println(e.getMessage());
-                context.status(500).html("""
-                        <div style='text-align:center; padding-top:50px; font-family:sans-serif;'>
-                            <h2>System Maintenance</h2>
-                            <p>My portfolio database is refreshing. Please wait a brief moment and
-                            reload.</p>
-                        </div>
-                        """);
+                context.status(500).html("<h2>Database Error</h2>");
+            });
+            config.routes.exception(Exception.class, (e, context) -> {
+                System.err.println(e.getMessage());
+                context.status(500).html("<h2>Application Error</h2>");
             });
         }).start(JAVALIN_PORT);
     }
